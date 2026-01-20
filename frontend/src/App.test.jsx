5e7c926272266b2254b556cc712e2bcfb8157d63
Trangtrang-013539 <119144080+Trangtrang-013539@users.noreply.github.com>
2026-01-20 11:25:46 +0700

    Add Vitest tests

---
+++ frontend/src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+
+test('App renders without crashing', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+})
